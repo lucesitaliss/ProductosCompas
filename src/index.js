1adb02d2d70f7c0d320192ef21241523f2536aef
lissette <ing.liss87@gmail.com>
2022-09-25 13:46:12 -0300

    cors configuration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 const categoriarouter = require('./routes/categorias.routes')
+
 const app = express()
 
-app.use(express.json())
+app.use(cors())
 app.use(morgan('dev'))
+app.use(express.json())
+
 app.use(categoriarouter)
-app.use((err, req, res, next) =>{
-    return res.json({message: err.message })
+app.use((err, req, res, next) => {
+  return res.json({ message: err.message })
 })
 
 app.listen(4000)
