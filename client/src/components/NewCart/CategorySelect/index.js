ad1edaadee9aca4878a624e101731e4fdc885b3c
lissette <ing.liss87@gmail.com>
2022-10-05 00:05:28 -0300

    Conectin  <select> with BD

--- client/src/components/NewCart/CategorySelect/index.js
+++ client/src/components/NewCart/CategorySelect/index.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function CategorySelect() {
+  const [categorys, setCategorys] = useState([])
+
+  const GetCategorys = async () => {
+    const response = await fetch('http://www.localhost:4000/categorys')
+    const result = await response.json()
+    setCategorys(result)
+  }
+
+  useEffect(() => {
+    GetCategorys()
+  }, [])
+
+  //setCategorys(GetCategorys)
+  // console.log(GetCategorys())
+  // const prueba = categorys.filter(
+  //   (category) => category.name_category == 'Aseo',
+  // )
   return (
-    <div>CategorySelect</div>
+    <div>
+  
+      <select >
+        {categorys.map((category) =>(
+
+          <option>{category.name_category}</option>
+        ))}
+        
+      
+      </select>
+    </div>
   )
 }
